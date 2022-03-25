50721e8203d2168b30bd46ace42fe26891d937ea
stonesha <stoneshaus@gmail.com>
2022-03-25 00:06:37 -0700

    fixed formatting on mobile

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,19 @@
 <script>
-  import Util from '../components/Util.svelte'
+	import Util from '../components/Util.svelte'
 </script>
 
 <svelte:head>
-  <title>utils by Stone Sha</title>
+	<title>utils by Stone Sha</title>
 </svelte:head>
 
-<h1 class="mb-4 font-mono font-bold text-6xl text-center text-slate-200">utils.stonesha.com</h1>
+<h1 class="mb-4 font-mono font-bold text-2xl sm:text-4xl md:text-6xl text-center text-slate-200">
+	utils.stonesha.com
+</h1>
 
-<div class="grid grid-cols-4 gap-2">
-  <Util title='JSON Validator and Formatter' slug='json-validator-and-formatter'/>
-  <Util title='URL Encoder and Decoder' slug='url-encoder-and-decoder'/>
-  <Util title='DND Class and Stats Generator' slug='dnd-generator'/>
+<div class="flex flex-row justify-center">
+	<div class="flex flex-col md:flex-row">
+		<Util title="JSON Validator and Formatter" slug="json-validator-and-formatter" />
+		<Util title="URL Encoder and Decoder" slug="url-encoder-and-decoder" />
+		<Util title="DND Class and Stats Generator" slug="dnd-generator" />
+	</div>
 </div>
